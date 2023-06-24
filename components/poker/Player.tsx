3ac2590464f188e12e5013eedf318fc64de0814d
ddmuzyk <domino2k1@gmail.com>
2023-06-24 17:18:10 +0200

    styling player component

--- components/poker/Player.tsx
+++ components/poker/Player.tsx
@@ -1,6 +1,14 @@
+import React, {use, useState} from "react";
+import styles from './Player.module.scss';
+
 const Player = () => {
+
+  const [money, setMoney] = useState(1000);
+
   return (
-    <div></div>
+    <div className={styles.player1}>
+      <p className={styles.container}>Player</p>
+    </div>
   )
 }
 
--- pages/game/poker.tsx
+++ pages/game/poker.tsx
@@ -1,14 +1,44 @@
 import Link from "next/link";
-import { ReactComponentElement } from "react";
+import { ReactComponentElement, useState, useEffect } from "react";
 import styles from './poker.module.scss';
 import Layout from "@/components/layout";
+import Player from "@/components/poker/Player";
+
+interface Player {
+  id: number,
+  name: string,
+  turn: boolean,
+  money: number
+}
 
 const Poker = (): JSX.Element => {
+
+  const [players, setPlayers] = useState<Array<Player>>([]);
+
+  useEffect(() => {
+    let newPlayers = [];
+    for (let i = 1; i < 5; i++) {
+      newPlayers.push({
+        id: i,
+        name: `Player${i}`,
+        turn: false,
+        money: 1000
+      })
+    }
+    setPlayers(newPlayers);
+  }, [])
+
+  console.log(players)
+
   return (
     <Layout siteTitle="Poker">
       <div className={styles.game}>
         <div className={styles.tableContainer}>
-          <div className={styles.table}></div>
+          <div className={styles.table}>
+            {players.map((player) => {
+              return <Player key={player.name}/>
+            })}
+          </div>
         </div>
       </div>
     </Layout>
