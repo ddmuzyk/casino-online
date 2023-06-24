bfbce31312c0b76e6f6d74b68e316e02f60af6ed
ddmuzyk <domino2k1@gmail.com>
2023-06-24 14:48:05 +0200

    styling poker table

--- pages/game/poker.tsx
+++ pages/game/poker.tsx
@@ -7,7 +7,9 @@ const Poker = (): JSX.Element => {
   return (
     <Layout siteTitle="Poker">
       <div className={styles.game}>
-        <h1 className={styles.title}>POKEREK</h1>
+        <div className={styles.tableContainer}>
+          <div className={styles.table}></div>
+        </div>
       </div>
     </Layout>
   )
