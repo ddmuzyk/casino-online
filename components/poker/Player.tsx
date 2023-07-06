af812106504d950e4bbee80d9b15a6a6101790f9
ddmuzyk <domino2k1@gmail.com>
2023-07-06 14:33:17 +0200

    adding randomlyGiveBlind function

--- components/poker/Player.tsx
+++ components/poker/Player.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 //   state: object
 // }
 
-const Player: React.FC<PlayerObject>  = ({id, name, turn, money, cards}) => {
+const Player: React.FC<PlayerObject>  = ({id, name, turn, money, cards, smallBlind}) => {
 
   const classname = `player${id}`;
 
@@ -15,7 +15,7 @@ const Player: React.FC<PlayerObject>  = ({id, name, turn, money, cards}) => {
     <div className={`${styles[classname]} ${styles['player-container']}`}>
       <div className={styles['imgs-container']}>
         {cards.map((card: string) => {
-          return <Image className={styles.image} key={card} src={`/svg-cards/${card}.svg`} alt="Playing card" width={77} height={154}></Image>
+          return <Image className={styles.image} key={card} src={`/svg-cards/${card}.svg`} alt="Playing card" width={77} height={154} priority={true}></Image>
         })}
       </div>
       <div className={styles.stats}>
--- pages/api/eval.ts
+++ pages/api/eval.ts
@@ -2,5 +2,6 @@ import * as PokerEvaluator from 'poker-evaluator-ts'
 
 
 export default function handler(req: any, res: any) {
-  res.status(200).json(PokerEvaluator.evalHand(['Ac', 'As', '2c']))
+  const {cards} = req.body;
+  res.status(200).json(PokerEvaluator.evalHand(cards))
 }
