570945cf6911ac733d2fd42082db128818500106
ddmuzyk <domino2k1@gmail.com>
2023-08-23 15:36:14 +0200

    added transitions for cards unmounting

--- pages/game/poker.tsx
+++ pages/game/poker.tsx
@@ -55,6 +55,7 @@ const Poker = (): JSX.Element => {
   const [tableMoney, setTableMoney] = useState(0); // Money on the table in the current round
   const [turn, setTurn] = useState<NumOrNull>(2); // Id of the player whose turn it is, randomly chosen at the start of the game
   const [communityCards, setCommunityCards] = useState<Array<string>>([]); // Community cards on the table
+  const [cardsVisible, setCardsVisible] = useState(true); // Boolean that checks if the cards are visible or not
   const [currentStage, setCurrentStage] = useState<Stage>('pre-flop'); // Current stage of the game
   // const [didGameStart, setDidGameStart] = useState(false); // Boolean that checks if the game has started
   // const [cardsClassname, setCardsClassname] = useState('hidden'); // Classname of the cards (hidden or visible)
@@ -267,10 +268,14 @@ const Poker = (): JSX.Element => {
         const winners = getArrayOfWinners(playersCopy);
         playersCopy = giveMoneyToWinners(playersCopy, winners, pot);
         setPot(() => 0);
-        setCommunityCards(() => []);
-        setPlayers(() => playersCopy);
-        console.log('winners: ', winners);
         setTurn(() => null);
+        setPlayers(() => playersCopy);
+        await sleep(1000);
+        setCardsVisible(() => false);
+        await sleep(1000);
+        setCommunityCards(() => []);
+        setCardsVisible(() => true);
+        console.log('winners: ', winners);
       } else {
         setCardsAreDealt(() => true);
         playersCopy = resetRoundState(playersCopy);
@@ -501,11 +506,11 @@ const Poker = (): JSX.Element => {
             }} 
             className={styles.table}>
             <div className={styles.pot}>Pot: <span className={styles.potValue} key={pot}>{pot}$</span></div>
-            <div className={styles.communityCards}>
+            <div className={`${styles.communityCards} ${cardsVisible ? "" : styles.communityCardsHidden}`}>
               {communityCards.map((card, id) => {
                 return (
                 <CSSTransition
-                in={true}
+                in={communityCards.length > 0}
                 timeout={200}
                 classNames={{
                   appearDone: styles.imageAppearDone,
