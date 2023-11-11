a9304857cf585786a41a44f8b07dd0b473ce824a
ddmuzyk <domino2k1@gmail.com>
2023-11-11 15:52:28 +0100

    added buttons to better control the bet amount

--- components/poker/Slider/Slider.tsx
+++ components/poker/Slider/Slider.tsx
@@ -5,7 +5,7 @@ interface SliderProps {
   step: string,
   max: number,
   betValue: string,
-  setBetValue: React.Dispatch<React.SetStateAction<string>>
+  setBetValue: React.Dispatch<React.SetStateAction<string>>,
 }
 
 const Slider: React.FC<SliderProps> = ({step, max, betValue, setBetValue}) => {
@@ -15,10 +15,19 @@ const Slider: React.FC<SliderProps> = ({step, max, betValue, setBetValue}) => {
     setBetValue(e.target.value);
   }
 
+  
+
+
   return (
     <div>
       <input className={styles.input} type="range" min="0" max={max} step={step} value={betValue} onChange={handleChange}/>
-      <p className={styles.value}>{betValue}$</p>
+      <div className={styles.valuesContainer}>
+        <p className={styles.value}>{betValue}$</p>
+        <span className={styles.row}>
+          <p className={`${styles.value} ${styles.btn}`} onClick={() => setBetValue(String(parseInt(betValue) - parseInt(step)))}>-</p>
+          <p className={`${styles.value} ${styles.btn}`} onClick={() => setBetValue(String(parseInt(betValue) + parseInt(step)))}>+</p>
+        </span>
+      </div>
     </div>
   )
 }
