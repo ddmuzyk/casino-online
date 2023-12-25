65444a1a86f218cf0b0a5c84a829f25b24481261
ddmuzyk <domino2k1@gmail.com>
2023-12-25 22:18:21 +0100

    fixed setInitialValues

--- tests/lib/poker/functions/blind.test.ts
+++ tests/lib/poker/functions/blind.test.ts
@@ -5,31 +5,61 @@ import { PlayerObject, EvaledHand } from '@/pages/game/poker.tsx';
 import { decky } from "@/lib/poker/poker-logic/poker";
 //
 
-describe('Blind', () => {
-  test('giveBlind', () => {
-    const mockPlayers: PlayerObject[] = [] 
-    const mockDeck = [...decky]
-    for (let i = 0; i < 3; i++) {
-      let playerCards = [mockDeck.pop(), mockDeck.pop()]; 
-      mockPlayers.push({ 
-        id: i,
-        name: `Player${i}`,
-        money: i == 0 ? 2000 : 1000,
-        cards: playerCards as Array<string>,
-        action: '-',
-        evaledHand: {} as EvaledHand,
-        smallBlind: 0,
-        bigBlind: 0,
-        bet: 0,
-        hasFolded: false,
-        won: false,
-        out: false,
-      })
-    }
+const mockCreatePlayers = (numberOfPlayers: number, currentDealerId: number, moneyAmount: number) => {
+  const mockPlayers: PlayerObject[] = [] 
+  const mockDeck = [...decky]
+  for (let i = 0; i < numberOfPlayers; i++) {
+    let playerCards = [mockDeck.pop(), mockDeck.pop()]; 
+    mockPlayers.push({ 
+      id: i,
+      name: `Player${i}`,
+      money: moneyAmount,
+      cards: playerCards as Array<string>,
+      action: '-',
+      evaledHand: {handName: 'flush', handRank: 2950, handType: 1,  value: 1930,} as EvaledHand,
+      smallBlind: 0,
+      bigBlind: 0,
+      bet: 0,
+      hasFolded: false,
+      won: false,
+      out: false,
+    })
+  }
+  return mockPlayers
+}
+
+describe('giveBlind', () => {
+  test('3 players, full money', () => {
+    const mockPlayers = mockCreatePlayers(3, 0, 1000)
     const newPlayers = giveBlind(mockPlayers, 5, 0)
-    expect(newPlayers[0].money).toBe(2000)
+    expect(newPlayers[0].money).toBe(1000)
     expect(newPlayers[1].money).toBe(995)
     expect(newPlayers[2].money).toBe(990)
+  })
+
+  test('3 players, not full money', () => {
+    const mockPlayers = mockCreatePlayers(3, 0, 1000)
+    mockPlayers[1].money = 3
+    let newPlayers = giveBlind(mockPlayers, 5, 0)
+    expect(newPlayers[0].money).toBe(1000)
+    expect(newPlayers[1].money).toBe(0)
+    expect(newPlayers[2].money).toBe(990)
+  })
+
+  test('2 players, full money', () => {
+    const mockPlayers = mockCreatePlayers(2, 0, 1000)
+    const newPlayers = giveBlind(mockPlayers, 5, 0)
+    expect(newPlayers[0].money).toBe(995)
+    expect(newPlayers[1].money).toBe(990)
+  })
+
+  test('2 players, not full money', () => {
+    const mockPlayers = mockCreatePlayers(2, 0, 1000)
+    mockPlayers[0].money = 3
+    mockPlayers[1].money = 3
+    const newPlayers = giveBlind(mockPlayers, 5, 0)
+    expect(newPlayers[0].money).toBe(0)
+    expect(newPlayers[1].money).toBe(0)
   })
   
 })
