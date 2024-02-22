57fe7998e51c153ee2725610614a5138e6b43940
ddmuzyk <domino2k1@gmail.com>
2024-02-22 15:42:29 +0100

    added registration form

--- components/poker/Register/Register.tsx
+++ components/poker/Register/Register.tsx
@@ -1,48 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import styles from "./Register.module.scss";
+import Close from "./Close/Close";
+
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
 
-export default function Register() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+interface RegisterProps {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  isOpen: boolean
+}
+
+export default function Register({setIsOpen}: RegisterProps, isOpen: boolean) {
 
   return (
-    <>
-      <Button onPress={onOpen}>Open Modal</Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-              <ModalBody>
-                <p> 
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
-                  dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. 
-                  Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. 
-                  Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur 
-                  proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
+    <div className={styles.main}>
+      <form className={`${styles.form} ${isOpen ? styles.visible : ""}`}>
+        <Close setIsOpen={setIsOpen}/>
+        <input placeholder='Username' className={styles.input} type="text"/>
+        <input placeholder="Email" className={styles.input} type="email"/>
+        <input placeholder='Password' className={styles.input} type="password"/>
+        <input placeholder='Confirm Password' className={styles.input} type="password"/>
+        <button type='button' className={styles.button}>Register</button>
+      </form>
+    </div>
   );
 }
---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  // const authTokens = request.cookies.get('authToken')
+  // if (!authTokens) {
+  //   return NextResponse.redirect(new URL('/', request.url))
+  // }
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/game/poker',
+}
