986cfae4118a84166d467c381623a2d03467430b
ddmuzyk <domino2k1@gmail.com>
2024-03-25 23:00:03 +0100

    working on user profile

--- pages/[user].tsx
+++ pages/[user].tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router'
+import styles from './[user].module.scss'
 import { getUserData } from '@/lib/poker/poker-logic/functions/requests'
 import { Props } from './game/poker';
+import Layout from '@/components/layout';
 
 export const getServerSideProps = async (context:any) => {
   const response = await getUserData(context);
@@ -18,11 +20,15 @@ export const getServerSideProps = async (context:any) => {
 export default function User({data}: Props) {
   const router = useRouter()
   const { user } = router.query
+  console.log(data)
 
   return (
-    <div>
-      <p>User: {user}</p>
-      <p>Money: {data.money}</p>
-    </div>
+    <Layout siteTitle={`${data.name} | Casino Online`}>
+      <main className={styles.main}>
+        <img src="poker/images/gambler.png" alt="Profile photo" className={styles.profile_picture} />
+        <p>User: {user}</p>
+        <p>Money: {data.money}</p>
+      </main>
+    </Layout>
   )
 }
